cfb170808f095b4ae0ec98e8120d2b685565f6fb
Oleg Montak <me@olegm-MacBook-Pro.local>
2022-08-08 23:57:10 +0300

    update vim

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 dotfiles_dir=~/dotfiles
 
-ln -sh $dotfiles_dir/vim ~/.vim
+ln -sh $dotfiles_dir/vim ~/
+mv ~/vim ~/.vim
 ln -sh $dotfiles_dir/vim/rc ~/.vimrc
 ln -sh $dotfiles_dir/zsh/rc ~/.zshrc
 ln -sh $dotfiles_dir/tig/rc ~/.tigrc

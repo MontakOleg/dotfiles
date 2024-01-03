943e3f9c5107a5a328a3713799dc4cce0525b976
Oleg Montak <oleg.montak@gmail.com>
2024-01-03 23:28:47 +0200

    update rtx location

--- fish/config.fish
+++ fish/config.fish
@@ -2,9 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Setup brew
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
 # Setup rtx
-~/bin/rtx activate fish | source
+~/.local/bin/rtx activate fish | source
 

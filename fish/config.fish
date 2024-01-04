488cb728ce2ccdbb553580f92504a459d9b9c53d
Oleg Montak <oleg.montak@gmail.com>
2024-01-04 14:49:18 +0200

    use mise

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-# Setup rtx
-~/.local/bin/rtx activate fish | source
+# Setup mise
+~/.local/bin/mise activate fish | source
 

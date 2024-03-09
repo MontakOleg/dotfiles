19b910e4125e4d9090bd015e121aad3274c4be44
Oleg Montak <oleg.montak@gmail.com>
2024-03-10 00:32:11 +0200

    ivy options

--- emacs/init.el
+++ emacs/init.el
@@ -52,6 +52,8 @@
 (use-package ivy
   :diminish
   :config
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-extra-directories nil)
   (ivy-mode 1))
 
 ;;;
@@ -61,7 +63,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(ivy projectile)))
+ '(package-selected-packages '(diminish ivy projectile)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

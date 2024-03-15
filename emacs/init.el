2427c687ea24c3a9f9a7b8f2884ddb976f593d41
Oleg Montak <oleg.montak@gmail.com>
2024-03-16 00:50:49 +0200

    more emacs settings

--- emacs/init.el
+++ emacs/init.el
@@ -29,11 +29,18 @@
 (setq history-length 25)
 (savehist-mode 1)
 
+;; Autosave right in files, not in #files#
+(auto-save-visited-mode 1)
+
+;; Don't pop up UI dialogs when prompting
+(setq use-dialog-box nil)
+
 ;;; Packages
 
 (require 'package)
 
-(setq package-archives '(("melpa" . "http://melpa.org/packages/")))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
 (package-initialize)
 (unless package-archive-contents (package-refresh-contents))
 
@@ -57,6 +64,12 @@
   (ivy-mode 1)
   (counsel-mode 1))
 
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1)
+  :config
+  (setq ivy-format-function #'ivy-format-function-line))
+
 ;; swift
 
 (use-package swift-mode)
@@ -65,6 +78,51 @@
 
 (use-package yaml-mode)
 
+;; helpful
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+
+;; magit
+
+(use-package magit
+  :custom
+  (magit-display-buffer-function 'magit-display-buffer-same-window-except-diff-v1))
+
+;;;
+
+(defun duplicate-line-or-region (&optional n)
+  "Duplicate current line, or region if active.
+    With argument N, make N copies.
+    With negative N, comment out original line and use the absolute value."
+  (interactive "*p")
+  (let ((use-region (use-region-p)))
+    (save-excursion
+      (let ((text (if use-region        ;Get region if active, otherwise line
+                      (buffer-substring (region-beginning) (region-end))
+                    (prog1 (thing-at-point 'line)
+                      (end-of-line)
+                      (if (< 0 (forward-line 1)) ;Go to beginning of next line, or make a new one
+                          (newline))))))
+        (dotimes (i (abs (or n 1)))     ;Insert N times, or once if not specified
+          (insert text))))
+    (if use-region nil                  ;Only if we're working with a line (not a region)
+      (let ((pos (- (point) (line-beginning-position)))) ;Save column
+        (if (> 0 n)                             ;Comment out original with negative arg
+            (comment-region (line-beginning-position) (line-end-position)))
+        (forward-line 1)
+        (forward-char pos)))))
+
+(global-set-key (kbd "s-d") 'duplicate-line-or-region)
+(global-set-key (kbd "s-/") 'comment-or-uncomment-region)
+
 ;;;
 
 (custom-set-variables
@@ -72,7 +130,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(flycheck yaml-mode swift-mode counsel diminish ivy)))
+ '(package-selected-packages
+   '(magit helpful ivy-rich flycheck yaml-mode swift-mode counsel diminish ivy)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

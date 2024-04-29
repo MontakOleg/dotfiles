a54bc39d1dabf52d3f243925c46fadc2217f8963
Oleg Montak <oleg.montak@gmail.com>
2024-04-29 03:26:16 +0300

    setup ripgrep

--- install.sh
+++ install.sh
@@ -14,3 +14,4 @@ ln -sh $dotfiles_dir/fish ~/.config/fish
 ln -sh $dotfiles_dir/emacs/init.el ~/.emacs.d/init.el
 ln -sh $dotfiles_dir/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
 ln -sh $dotfiles_dir/bin/mitm_start.sh ~/.local/bin/mitm_start.sh
+ln -sh $dotfiles_dir/ripgreprc ~/.config/ripgreprc

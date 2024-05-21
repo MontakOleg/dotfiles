ca2ffabc55162320d8b2396980e35e911114450f
Oleg Montak <oleg.montak@gmail.com>
2024-05-21 23:25:53 +0300

    [emacs] mc use mark-next-like-this

--- emacs/init.el
+++ emacs/init.el
@@ -219,8 +219,8 @@
 
 (use-package multiple-cursors
   :bind (("C-S-c C-S-c" . 'mc/edit-lines)
-         ("C->" . 'mc/mark-next-like-this-word)
-         ("C-<" . 'mc/mark-previous-like-this-word)
+         ("C->" . 'mc/mark-next-like-this)
+         ("C-<" . 'mc/mark-previous-like-this)
          ("C-c C-<" . 'mc/mark-all-like-this-dwim)
          :map mc/keymap
          ("<return>" . nil)

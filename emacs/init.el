13e54306ecfbb9a244de7fd740b9ce39f209c7ec
Oleg Montak <oleg.montak@gmail.com>
2024-07-28 00:15:01 +0300

    [emacs] add ob-swift

--- emacs/init.el
+++ emacs/init.el
@@ -216,7 +216,8 @@
 ;; swift
 
 (use-package swift-mode
-  :mode ("\\.swift\\'" . swift-mode))
+  :mode ("\\.swift\\'" . swift-mode)
+  :interpreter "swift")
 
 (use-package eglot
   :custom
@@ -224,6 +225,16 @@
   :config
   (add-to-list 'eglot-server-programs
                '(swift-mode . my-swift-mode:eglot-server-contact)))
+
+(use-package ob-swift
+  :after org)
+
+;; org
+
+(use-package org
+  :ensure nil
+  :custom (org-confirm-babel-evaluate nil))
+
 ;; yaml
 
 (use-package yaml-mode
@@ -376,7 +387,7 @@ installation and return its path."
  '(custom-safe-themes
    '("c7a926ad0e1ca4272c90fce2e1ffa7760494083356f6bb6d72481b879afce1f2" "c1638a7061fb86be5b4347c11ccf274354c5998d52e6d8386e997b862773d1d2" default))
  '(package-selected-packages
-   '(kotlin-ts-mode editorconfig copilot quelpa-use-package quelpa exec-path-from-shell languagetool markdown-mode vterm embark-consult embark orderless consult marginalia vertico modus-themes wgrep dumb-jump multiple-cursors expand-region evil-nerd-commenter company magit helpful yaml-mode swift-mode)))
+   '(ob-swift kotlin-ts-mode editorconfig copilot quelpa-use-package quelpa exec-path-from-shell languagetool markdown-mode vterm embark-consult embark orderless consult marginalia vertico modus-themes wgrep dumb-jump multiple-cursors expand-region evil-nerd-commenter company magit helpful yaml-mode swift-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

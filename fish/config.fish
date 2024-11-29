f949f038e27c8b62cb55429f201fd77bbcce420a
Oleg Montak <oleg.montak@gmail.com>
2024-11-29 14:43:20 +0200

    add emc abbr to run emacsclient

--- fish/config.fish
+++ fish/config.fish
@@ -2,6 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+abbr --add emc emacsclient -n
+
+set -gx LC_ALL en_US.UTF-8
+set -gx LANG en_US.UTF-8
+
 # Setup mise
 ~/.local/bin/mise activate fish | source
 

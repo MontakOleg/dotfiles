04e74ab5d451f315dfc6ac30ab208ef7da4be35c
Oleg Montak <oleg.montak@gmail.com>
2025-02-16 16:42:14 +0200

    setup path explicitly

--- fish/config.fish
+++ fish/config.fish
@@ -7,6 +7,16 @@ abbr --add emc emacsclient -n
 set -gx LC_ALL en_US.UTF-8
 set -gx LANG en_US.UTF-8
 
+set -gx PATH \
+    /Users/me/.local/share/mise/shims \
+    /Users/me/go/bin \
+    /Users/me/.local/bin \
+    /Users/me/.mint/bin \
+    /Users/me/dev/other/git-pile/bin \
+    /usr/local/bin \
+    /opt/homebrew/bin \
+    $PATH
+
 # Setup mise
 ~/.local/bin/mise activate fish | source
 

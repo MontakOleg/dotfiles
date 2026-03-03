7cec5ab09dcdcbd99ce022449b25b12fe8f732b8
Oleg Montak <oleg.montak@gmail.com>
2026-03-03 15:02:40 +0200

    add android sdk

--- fish/config.fish
+++ fish/config.fish
@@ -8,6 +8,19 @@ abbr --add noquar /usr/bin/xattr -r -d com.apple.quarantine
 set -gx LC_ALL en_US.UTF-8
 set -gx LANG en_US.UTF-8
 
+# android
+set -gx ANDROID_SDK_ROOT "$HOME/Library/Android/sdk"
+set -gx ANDROID_HOME "$ANDROID_SDK_ROOT"
+fish_add_path -g "$ANDROID_SDK_ROOT/emulator" "$ANDROID_SDK_ROOT/platform-tools" "$ANDROID_SDK_ROOT/cmdline-tools/latest/bin"
+# android end
+
+# pnpm
+set -gx PNPM_HOME "/Users/me/Library/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
 set -gx PATH \
     /Users/me/.local/share/mise/shims \
     /Users/me/go/bin \
@@ -25,11 +38,3 @@ bind \cx\ce edit_command_buffer
 
 # Setup mise
 ~/.local/bin/mise activate fish | source
-
-
-# pnpm
-set -gx PNPM_HOME "/Users/me/Library/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end

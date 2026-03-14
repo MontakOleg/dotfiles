c1c5b3f52be6937bc9369b5014753180863720c8
Oleg <me@Olegs-MBP.fritz.box>
2026-03-14 16:02:37 +0200

    remove starship and kitty

--- install.sh
+++ install.sh
@@ -10,9 +10,7 @@ ln -sh $dotfiles_dir/zsh/profile ~/.zprofile
 ln -sh $dotfiles_dir/zsh/zshenv ~/.zshenv
 ln -sh $dotfiles_dir/tig/rc ~/.tigrc
 ln -sh $dotfiles_dir/ruby/gemrc ~/.gemrc
-ln -sh $dotfiles_dir/kitty ~/.config/kitty
 ln -sh $dotfiles_dir/ghostty ~/.config/ghostty
-ln -sh $dotfiles_dir/starship/starship.toml ~/.config/starship.toml
 ln -sh $dotfiles_dir/fish ~/.config/fish
 ln -sh $dotfiles_dir/lazygit/config.yml ~/Library/Application\ Support/lazygit/config.yml
 ln -sh $dotfiles_dir/bin/mitm_start.sh ~/.local/bin/mitm_start.sh
